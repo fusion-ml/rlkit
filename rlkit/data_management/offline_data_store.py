653332a3db4b6830ceb818c828b4e479149cb6e5
IanChar <ianchar4@gmail.com>
2021-01-15 12:12:12 -0500

    Modified offline datastore.

--- rlkit/data_management/offline_data_store.py
+++ rlkit/data_management/offline_data_store.py
@@ -6,26 +6,19 @@ import numpy as np
 
 class OfflineDataStore(object):
 
-    def __init__(self, data, give_next_terminals=False):
+    def __init__(self, data):
         """Constructor.
         Args:
             data: The offline dataset. Takes data structures...
                 * tuple: List of tuples (s, a, r, next_s, done)
                 * dict: Dictionary with keys mapping to observation, etc
-            give_next_terminals: Whether to also include whether the next state
-                is terminal in batches.
         """
         if type(data) is dict:
-            self._observations = data['observations'][:-1]
-            self._actions = data['actions'][:-1]
-            self._rewards = data['rewards'][:-1]
-            self._next_obs = data['observations'][1:]
-            self._terminals = data['terminals'][:-1]
-            self._next_terminals = data['terminals'][1:]
-            self._next_terminals = np.logical_or(
-                    self._next_terminals,
-                    self._terminals,
-            )
+            self._observations = data['observations']
+            self._actions = data['actions']
+            self._rewards = data['rewards']
+            self._next_obs = data['next_observations']
+            self._terminals = data['terminals']
             self._size = self._observations.shape[0]
         else:
             self._size = len(data)
@@ -37,15 +30,6 @@ class OfflineDataStore(object):
             self._rewards = tup_elements[2].reshape(-1, 1)
             self._next_obs = tup_elements[3]
             self._terminals = tup_elements[4].reshape(-1, 1)
-            self._next_terminals = np.vstack([
-                self._terminals[1:],
-                np.ones((1, 1)),
-            ])
-            self._next_terminals = np.logical_or(
-                self._terminals,
-                self._next_terminals,
-            )
-        self._give_next_terminals = give_next_terminals
 
     def get_batch(self, batch_size):
         """Get a batch of the data."""
@@ -57,8 +41,4 @@ class OfflineDataStore(object):
             terminals=self._terminals[indices].reshape(-1, 1),
             next_observations=self._next_obs[indices],
         )
-        if self._give_next_terminals:
-            batch['next_terminals'] =\
-                    self._next_terminals[indices].reshape(-1, 1)
         return batch
-
